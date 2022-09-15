9f6c7d367f76fe75861803c9ed164f91d9328524
FadiHanna <fadi.hanna@rektron.se>
2022-09-15 03:35:40 +0200

    Add server docs

--- server/src/tools/docs/auth.ts
+++ server/src/tools/docs/auth.ts
@@ -1,6 +1,6 @@
 // Auth
 /**
- * @api {post} /login check login of a user
+ * @api {post} /login Login a user
  * @apiName Login
  * @apiGroup Auth
  * @apiHeaderExample {json} Header-Example:
@@ -9,11 +9,35 @@
  *     }
  * @apiBody {String} uname Username.
  * @apiBody {String} psw Password.
- * @apiSuccess {String} accessToken login accesstoken.
- * @apiSuccess {String} author  Token author.
+ * @apiSuccess {String} accessToken Login accesstoken.
+ * @apiSuccess {String} author Token author.
+ * @apiSampleRequest https://localhost:5000/login
  * @apiSuccessExample Success-Response:
  *     {
  *       "accessToken": "XXX",
  *       "author": "Erik"
  *     }
+ */
+
+/**
+ * @api {post} /register Register a user
+ * @apiName Register
+ * @apiGroup Auth
+ * @apiHeaderExample {json} Header-Example:
+ *                  {
+ *                    "Content-Type": "application/json"
+ *                  }
+ * @apiBody {String} uname Username.
+ * @apiBody {String} psw Password.
+ * @apiBody {String} repsw Retyped password.
+ * @apiBody {String} email Email-address.
+ * @apiBody {String} mobnr Mobile-number.
+ * @apiSuccess {String} accessToken Login accesstoken.
+ * @apiSuccess {String} author Token author.
+ * @apiSampleRequest https://localhost:5000/register
+ * @apiSuccessExample Success-Response:
+ *                    {
+ *                      "accessToken": "XXX",
+ *                      "author": "Erik"
+ *                    }
  */
