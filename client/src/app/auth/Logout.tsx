806a7b2c647531c88bbb50244c25271d1c5f149e
Fadi Hanna <fadialias230@gmail.com>
2024-12-27 22:08:10 +0100

    Upgrade packages and make headersection more width

--- client/src/app/auth/Logout.tsx
+++ client/src/app/auth/Logout.tsx
@@ -1,18 +1,16 @@
-import { Navigate } from 'react-router-dom';
-import React from 'react';
-
 // Components
-import { doLogOut } from '../../functions';
 import useReduxConsts from '../../hooks/useReduxConsts';
+import { doLogOut } from '../../functions';
+import { Navigate } from 'react-router-dom';
 
 /**
  * @author Fadi Hanna<fhanna181@gmail.com>
  */
 
-const Logout: React.FC = () => {
+const Logout = () => {
   const { login, dispatch } = useReduxConsts();
 
-  return <>{login ? doLogOut(dispatch) : <Navigate to='/' />}</>;
+  return login ? doLogOut(dispatch) : <Navigate to='/' />;
 };
 
 export default Logout;
